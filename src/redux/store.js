ac5e8c1357d681d1125368050edb7310af0fc6b7
TAHAWY <amer.zp582@gmail.com>
2022-04-22 17:58:59 +0200

    a

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./counterSlice";
+import userSlice from "./userSlice";
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 const store = configureStore(
   {
     reducer: {
-      counter: counterSlice,
+      user: userSlice,
     },
   },
   enhancer()
---
+++ src/redux/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "counter",
+  initialState: {
+    name: "",
+    email: "",
+  },
+  reducers: {
+    addUser: (state, action) => {
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+    },
+  },
+});
+
+export const { addUser } = userSlice.actions;
+
+export default userSlice.reducer;
